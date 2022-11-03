90334e975d95802f76760096ccd3924ea407efe4
mahalakshmi <chikkuboye@gmail.com>
2022-11-03 17:28:29 +0530

    fetch

--- api1.py
+++ api1.py
@@ -8,14 +8,14 @@ try:
 except mysql.connector.Error as e:
     sys.exit('connection failure')
 mycursor = mydb.cursor()
-data = requests.get("https://jsonplaceholder.typicode.com/users").text
+data = requests.get("https://jsonplaceholder.typicode.com/posts").text
 
 data_info = json.loads(data)
-#user_list = []
 #print(data_info)
 for i in data_info:
-    sql = "INSERT INTO `user`(`Name`, `Address`, `Phone_num`) VALUES ('"+i['name']+"','"+i['email']+"','"+i['phone']+"')"
+    id = str(i['userId'])
+    sql = "INSERT INTO `post`(`User_Id`, `title`, `body`) VALUES ('"+id+"','"+i['title']+"','"+i['body']+"')"
     mycursor.execute(sql)
     mydb.commit()
-    print('Data inserted Successfully',i['name'])
-#print(user_list)
+    print('Inserted succesfully',id)
+
